7c97c3af76fe37a8a6d42ba1c6809acdb68f613e
Saniya <saniyaasreen196@gmail.com>
2026-05-16 19:50:47 +0530

    Fix map lookup and official AQI flow

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
+
+const apiBaseUrl = process.env.REACT_APP_API_URL || 'https://breathtruth.onrender.com';
+axios.defaults.baseURL = apiBaseUrl;
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
